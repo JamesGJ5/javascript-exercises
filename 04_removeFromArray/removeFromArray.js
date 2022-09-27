eed24d67a086eb7b4ea04981b5d7d533d86340d9
James Graca-Jones <jamesgracajones@gmail.com>
2022-09-27 21:03:27 +0100

    Pass 'removes a single value' test
    In removeFromArray.js:
    - Allow function expression to have parameters of the array to be
    removed from and the value to be removed
    - Iterate through the array with a for loop and when the value is
    found, remove it using the array splice method

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, valueToRemove) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === valueToRemove) {
+            array.splice(i, 1)
+        };
+    };
+    return array;
 };
 
 // Do not edit below this line
