68983d3901d56eec4bc589a02d7ec02dd0963927
James Graca-Jones <jamesgracajones@gmail.com>
2022-09-28 14:28:00 +0100

    Enable function to pass 'works with larger number first' test
    In sumAll.js:
    - When calculating the number of integers in the range, calculate the
    absolute value of (the second passed integer - the former passed
    integer), since the order will affect this calculation but not the
    midpoint calculation

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,5 @@
 const sumAll = function(num1, num2) {
-    return (num2 - num1 + 1) * (num1 + num2) / 2;
+    return (Math.abs(num2 - num1) + 1) * (num1 + num2) / 2;
 };
 
 // Do not edit below this line
