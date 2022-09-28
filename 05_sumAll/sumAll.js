b67f88ea80a5c740ea69a8a7fd690df03e0b5079
James Graca-Jones <jamesgracajones@gmail.com>
2022-09-28 14:31:10 +0100

    Enable function to pass 'returns ERROR with negative numbers' test
    In sumAll.js:
    - Make if...else statement where 'ERROR' string is returned if either
    passed number is negative, otherwise return sum of range

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
 const sumAll = function(num1, num2) {
-    return (Math.abs(num2 - num1) + 1) * (num1 + num2) / 2;
+    if (num1 < 0 || num2 < 0) {
+        return 'ERROR';
+    } else {
+        return (Math.abs(num2 - num1) + 1) * (num1 + num2) / 2;
+    };
 };
 
 // Do not edit below this line
