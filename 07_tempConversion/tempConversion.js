a9c67e2772bdaeda5c207334dd2b4e105f93ea5e
James Graca-Jones <jamesgracajones@gmail.com>
2022-09-28 15:35:54 +0100

    Use arrow function for rounding to 1 decimal place

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,13 @@
+const round1DecimalPlace = x => Math.round(x * 10) / 10;
+
 const ftoc = function(f) {
   c = (f - 32) * 5 / 9;
-  return Math.round(c * 10) / 10;
+  return round1DecimalPlace(c);
 };
 
 const ctof = function(c) {
   f = (c * 9 / 5) + 32;
-  return Math.round(f * 10) / 10;
+  return round1DecimalPlace(f);
 };
 
 // Do not edit below this line
