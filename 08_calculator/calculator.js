c68e9955ee29cbe95f33e157cceda6ce61aad35e
James Graca-Jones <jamesgracajones@gmail.com>
2022-10-02 21:31:17 +0100

    Change the names of the sum() and multiply() parameters to numberArray

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,12 +6,12 @@ const subtract = function(num1, num2) {
 	return add(num1, -num2);
 };
 
-const sum = function(summedArray) {
-	return summedArray.reduce((cumulativeSum, addend) => cumulativeSum + addend, 0);
+const sum = function(numberArray) {
+	return numberArray.reduce((cumulativeSum, addend) => cumulativeSum + addend, 0);
 };
 
-const multiply = function(multipliedArray) {
-  return multipliedArray.reduce((cumulativeProduct, factor) => 
+const multiply = function(numberArray) {
+  return numberArray.reduce((cumulativeProduct, factor) => 
     cumulativeProduct * factor, 1
   );
 };
