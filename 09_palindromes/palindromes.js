53055c27c50546ef64b1321b759d80a8a177eab6
James Graca-Jones <jamesgracajones@gmail.com>
2022-10-02 21:51:00 +0100

    Allow palindromes() to pass 'works with single words' test

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    left = 0;
+    right = string.length - 1;
+    while (right >= left) {
+        if (string[left] === string[right]) {
+            left++;
+            right--;
+        } else {
+            return false;
+        };
+    };
+    return true;
 };
 
 // Do not edit below this line
