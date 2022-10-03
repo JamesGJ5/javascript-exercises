0ef499a769989950fc97bf8eab1e382e69ca4f08
James Graca-Jones <jamesgracajones@gmail.com>
2022-10-03 15:07:51 +0100

    Enable findTheOldest() to pass 'finds the oldest person!'
    Use the reduce() method, with, as the initial value, a person object
    with yearOfBirth and yearOfDeath as 0 (since the oldest person will be
    at least  0 when they die, so this initial value fits the comparison)

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(personList) {
+    return oldestPerson = personList.reduce((currentOldest, currentPerson) => 
+        ((currentOldest.yearOfDeath - currentOldest.yearOfBirth) < (currentPerson.yearOfDeath - currentPerson.yearOfBirth)) 
+            ? currentPerson : currentOldest
+        ,
+        {yearOfBirth: 0, yearOfDeath: 0}
+    );
 };
 
 // Do not edit below this line
