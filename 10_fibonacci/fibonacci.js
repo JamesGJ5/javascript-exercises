a05cbb235bb6c81d3d5eba965e31c76861712c60
James Graca-Jones <jamesgracajones@gmail.com>
2022-10-03 13:54:12 +0100

    Enable fibonacci() to pass tests up to '25th fibonacci number is 75025'
    In fibonacci():
    - Store penultimate and final (of Fibonacci sequence calculated so far)
    in an array, noting that preceding items will be neglected from memory
    - Create a for loop in which the contents of this array are added and
    the array is replaced with a new penultimate and new final, up to n - 2
    times

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let penultimateAndFinal = [1, 1];
+    for (let i = 3; i < n + 1; i++) {
+        const newPenultimate = penultimateAndFinal[1];
+        const newFinal = penultimateAndFinal[0] + newPenultimate;
+        penultimateAndFinal = [newPenultimate, newFinal];
+    };
+    return penultimateAndFinal[1];
 };
 
 // Do not edit below this line
