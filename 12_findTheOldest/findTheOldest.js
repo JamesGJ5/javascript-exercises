3aa10a9f8fceaa3d4b1ac9ba603db9f4947c7e7a
James Graca-Jones <jamesgracajones@gmail.com>
2022-10-03 15:46:14 +0100

    Enable findTheOldest() to pass the remaining two tests
    In findTheOldest():
    - Replace the initial value with [person, their maximum age during life]
    - Create variable to record latest year of life of current person,
    using their year at which the variable assignment is done otherwise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,10 +1,18 @@
 const findTheOldest = function(personList) {
-    return oldestPerson = personList.reduce((currentOldest, currentPerson) => 
-        ((currentOldest.yearOfDeath - currentOldest.yearOfBirth) < (currentPerson.yearOfDeath - currentPerson.yearOfBirth)) 
-            ? currentPerson : currentOldest
-        ,
-        {yearOfBirth: 0, yearOfDeath: 0}
+    const oldestPersonMaxAge = personList.reduce((currentOldest, currentPerson) => {
+            let latestYear;
+            if ("yearOfDeath" in currentPerson) {
+                latestYear = currentPerson.yearOfDeath;
+            } else {
+                latestYear = (new Date()).getFullYear();
+            };
+            const currentPersonMaxAge = latestYear - currentPerson.yearOfBirth;
+            return (currentPersonMaxAge > currentOldest[1]) 
+            ? [currentPerson, currentPersonMaxAge] : currentOldest
+        },
+        [null, 0]
     );
+    return oldestPersonMaxAge[0];
 };
 
 // Do not edit below this line
